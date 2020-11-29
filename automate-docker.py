8fe98e94ce085fae51e53db74dba0b16451e6a20
Siddhartha <61656756+SiddharthaShandilya@users.noreply.github.com>
2020-11-29 08:30:43 +0530

    Update automate-docker.py
    docker-module

--- automate-docker.py
+++ automate-docker.py
@@ -1,26 +1,18 @@
+
+
 import os
-os.system("clear")
-os.system("tput setaf 3")
 
-print("\t\t\t\tWelcome welcome welcome\t\t\n\n")
-
-os.system("tput setaf 5")
-
-print("\n\n\t\t\t\tWelcome to my menu program")
-
-
-
-
-#--------------------------->>>>>>>>>>>>>>>> DOCKER <<<<--------------------------
+# --------------------------->>>>>>>>>>>>>>>> DOCKER <<<<--------------------------
 
 
 #     docker()  #<<<<<<<----------- call docker() to start the program  END OF THE LINE
 
 
-#--------->>>>>>>>>>>>>>>>>  options <<<<<<<<<---------------------
+# --------->>>>>>>>>>>>>>>>>  options <<<<<<<<<---------------------
 
 
 dockerStart = 0   # <<<<----------this will tell us whether docker is started or not
+
 
 def option():
     choice = input("\n\t Enter 1 to start the docker \n \t Enter 2 to stop the docker \n \t Enter 3 to know the Details of docker \n \t Enter 4 for Configuring Docker image \n \t Enter 5 for Hosting web server on docker \n \t Enter x to exit the docker \n \t\n\t\t--------->>>>>\t")
@@ -28,82 +20,85 @@ def option():
     print("\n\n")
     return choice
 
+
 def Rechoice():
-    reChoice = input("\n\tyou have exitted from the service Enter R to re-enter and E to exit")
-    if(reChoice == 'R' ):
-            service()
+    reChoice = input(
+        "\n\tyou have exitted from the service Enter R to re-enter and E to exit")
+    if(reChoice == 'R'):
+        service()
     elif(reChoice == 'E'):
-            print("\n\tyou have choosen to withdraw \n \t\tHAPPY CODING")
-            
+        print("\n\tyou have choosen to withdraw \n \t\tHAPPY CODING")
+
     else:
         print("\t\t\t !!!    EXITING !!!!!!!")
         exit()
-        
-        
-#-------------------------->>>>>>>>>>>>>>    dockerHostWebServer()   <<<<<<--------------------
+
+
+# -------------------------->>>>>>>>>>>>>>    dockerHostWebServer()   <<<<<<--------------------
 
 def dockerHostWebServer():
     print("\n\tYou have chosen option 5\n")
-    osName  = input("\n\tEnter the name of the os \t --->  ")
-    
+    osName = input("\n\tEnter the name of the os \t --->  ")
+
     os.system("docker start {}".format(osName))
     os.system("docker ps --latest")
     print("\n \n")
 #    print("\n\t\t -->>Enter [ctrl+d] to exit the command line<<---\n")
   #  os.system()
     print("\n The ip of the os is \t --->\t ")
-    var = os.system("docker container inspect --format \"{{.NetworkSettings.IPAddress}}\" {}".format(osName))
+    var = os.system(
+        "docker container inspect --format \"{{.NetworkSettings.IPAddress}}\" {}".format(osName))
     os.system("docker exec -it {} yum install httpd".format(osName))
  #   print("\n\t\twrite the codes for the server to deploy\n")
   #  os.system("")
-    webFileName = input("\n\tEnter the name of the html file that needs to be launched\t--->\t")
-    os.system("docker cp {} {}:/var/www/html".format(webFileName,osName))
-    print("\n\t\t--->>>   LAUNCHING {} FILE ON  WEB SERVER on {}  <<<-----\t\n".format(webFileName,osName))
+    webFileName = input(
+        "\n\tEnter the name of the html file that needs to be launched\t--->\t")
+    os.system("docker cp {} {}:/var/www/html".format(webFileName, osName))
+    print("\n\t\t--->>>   LAUNCHING {} FILE ON  WEB SERVER on {}  <<<-----\t\n".format(webFileName, osName))
     os.system("docker exec -it {} /usr/bin/httpd".format(osName))
 
 
-#----------->>>>>>>>>> SERVICE() <<<<<<<<-__----------------
+# ----------->>>>>>>>>> SERVICE() <<<<<<<<-__----------------
 def service():
-        exitStatus = "dumycode"
-        print("\n\t \t Starting Docker \t\t\n")
-        os.system("systemctl start docker")
-        dockerStart = 1
-        ch = 1
-        while( exitStatus != "n"):
-            os.system("clear")
-            ch = option()
-            if(ch == "1"):
-                print("\n\t \t Starting Docker \t\t\n")
-                os.system("systemctl start docker")
-                dockerStart = 1
-                print("\n\tdocker is running")        
-            elif(ch == "2"):
-                print("\n\tstopping docker")
-                os.system("systemctl stop docker")
-                print("\n\tThe docker is stoped")
-                dockerStart = 0
-            elif(ch == "3"):
-                print("\n\t\tpresenting docker Details\n") 
-               # os.system("systemctl status docker")
-                showDockerDetails(dockerStart)   
-            elif(ch == "4"):
+    exitStatus = "dumycode"
+    print("\n\t \t Starting Docker \t\t\n")
+    os.system("systemctl start docker")
+    dockerStart = 1
+    ch = 1
+    while(exitStatus != "n"):
+        os.system("clear")
+        ch = option()
+        if(ch == "1"):
+            print("\n\t \t Starting Docker \t\t\n")
+            os.system("systemctl start docker")
+            dockerStart = 1
+            print("\n\tdocker is running")
+        elif(ch == "2"):
+            print("\n\tstopping docker")
+            os.system("systemctl stop docker")
+            print("\n\tThe docker is stoped")
+            dockerStart = 0
+        elif(ch == "3"):
+            print("\n\t\tpresenting docker Details\n")
+           # os.system("systemctl status docker")
+            showDockerDetails(dockerStart)
+        elif(ch == "4"):
 
-                # ==========>>>>>>> here we will ask the user details about the image it wants to run
-                configDockerImage(dockerStart)
-            elif(ch == "5"):
-                dockerHostWebServer()
-            elif(ch == "x"):
-                print("\n\t\tYou have chosen To exit")
-                
-                print("\n !!!! \t\tt\t\t\t\ EXITING THE SYSTEM  \t\t\tt\t !!!!")
-                break
-            else:
-                print("You have entered wrong choice")
-            
+            # ==========>>>>>>> here we will ask the user details about the image it wants to run
+            configDockerImage(dockerStart)
+        elif(ch == "5"):
+            dockerHostWebServer()
+        elif(ch == "x"):
+            print("\n\t\tYou have chosen To exit")
+
+            print("\n !!!! \t\tt\t\t\t\ EXITING THE SYSTEM  \t\t\tt\t !!!!")
+            break
+        else:
+            print("You have entered wrong choice")
+
 #            exitStatus = input("Enter y to Return to main menu \nEnter n  to exit [y/n] \n\t------>>\t")
-        
 
-        
+
 def configDockerImage(dockerStart):
     print("\t\t\t Welcome to docker configuration \n")
     if(dockerStart == 1):
@@ -123,14 +118,14 @@ def configDockerImage(dockerStart):
             if(choice == "E"):
                 print("\n\t\tReturning to main menu\n")
                 return
-    
+
     else:
         print("\n\tUnable to perofrm action , Docker is closed\n")
         print("\t \t Starting Docker \t\t\n")
         os.system("systemctl start docker")
         dockerStart = 1
-        
-        
+
+
 # --------------------->>>>>>>>> showDockerDetails()<<<<<<<<<<<<____
 def showDockerDetails(dockerStart):
     con = "Y"
@@ -138,93 +133,102 @@ def showDockerDetails(dockerStart):
         print("\n\tUnable to perofrm action , Docker is closed\n")
         print("\n\t\t Starting Docker \t\t\n")
         os.system("systemctl start docker")
-        dockerStart = 1 
-        
-    while(con !="E"):
+        dockerStart = 1
+
+    while(con != "E"):
         print("\n \t\t\t Below are the option")
         os.system("docker ps --help")
         option = input("\n\n\t Enter details \t--->>\t")
         os.system("docker ps {}".format(option))
-        con = input("\n\n\t\tEnter Y to return to option 3\n \t\tEnter E to go to Docker menu\n\t\t--------->>>>" )
-        
+        con = input(
+            "\n\n\t\tEnter Y to return to option 3\n \t\tEnter E to go to Docker menu\n\t\t--------->>>>")
+
     print("\n \t\t\t Exiting option Docker Details\n ")
-    
+
 # ==================>>>>>>>>Here we will ask the user details about the image it wants to pull
 
-def dockerRunImage(dockerStart):  
+
+def dockerRunImage(dockerStart):
     if(dockerStart == 1):
-        
+
         os.system("docker ps -a")
-        
-        imgPull = input("\n\nEnter the name of the image you want to run\t -------> \t")
+
+        imgPull = input(
+            "\n\nEnter the name of the image you want to run\t -------> \t")
         os.system("docker search {}".format(imgPull))
-        imgChoicePull = input("\nEnter the version of the image you want to run\t ---->> \t")
-        choiceDesiredName = input("\n\tDo you want to give a name to your image [y/n]\t -------->>\t")
-        
+        imgChoicePull = input(
+            "\nEnter the version of the image you want to run\t ---->> \t")
+        choiceDesiredName = input(
+            "\n\tDo you want to give a name to your image [y/n]\t -------->>\t")
+
         if(choiceDesiredName == "y"):
             print("\n\tRunning Docker image\t---->> {}".format(imgChoicePull))
-            configImageName(imgChoicePull)     
-        
+            configImageName(imgChoicePull)
+
         else:
-            print("\nENTER  -->>>  exit  <<----- to exit the image or press [ctrl+c] to exit")
-            os.system("docker run -i -t {}".format(imgChoicePull)) 
+            print(
+                "\nENTER  -->>>  exit  <<----- to exit the image or press [ctrl+c] to exit")
+            os.system("docker run -i -t {}".format(imgChoicePull))
         print("\n\t!!!!\tEXITING\t!!!!!!\n")
-    
+
     else:
         print("\n\t\t The docker has stooped\n")
-        
+
 # --------------------->>>>>>>>>>       configImageName() <<<<<<<<<<<<<------------------------
-        
+
+
 def configImageName(imgChoicePull):
     desiredImageName = input("\n\tEnter the name \t\n \t------>  ")
-    os.system("docker run -it --name {} {}".format(desiredImageName,imgChoicePull))
-           
-#-------------------------->>>>>>>>>>>>>>    deleteAllPullImage()   <<<<<<--------------------
+    os.system("docker run -it --name {} {}".format(desiredImageName, imgChoicePull))
 
-def dockerPullImage(dockerStart):        
+# -------------------------->>>>>>>>>>>>>>    deleteAllPullImage()   <<<<<<--------------------
+
+
+def dockerPullImage(dockerStart):
     if(dockerStart == 1):
-        imgStart = input("\n\tEnter the name of the image you want to Pull\t ------>\t")
+        imgStart = input(
+            "\n\tEnter the name of the image you want to Pull\t ------>\t")
         os.system("docker search {}".format(imgStart))
-        imgChoicePull = input("\n\tEnter the version of the image you want to Pull\t ---->> \t")
+        imgChoicePull = input(
+            "\n\tEnter the version of the image you want to Pull\t ---->> \t")
         print("\n \t\t\t\t !!!! PULLING IMAGE {} !!!!\n".format(imgChoicePull))
-        os.system("docker pull {}".format(imgChoicePull)) 
+        os.system("docker pull {}".format(imgChoicePull))
     else:
         print("\n\tThe docker has stooped")
-    return    
-    
+    return
+
+    # -------------------------->>>>>>>>>>>>>>    deleteDockerImage()   <<<<<<--------------------
 
 
-        #-------------------------->>>>>>>>>>>>>>    deleteDockerImage()   <<<<<<--------------------
-
-        
 def deleteDockerImage():
    # deleteImageName = ""
     chooseExit = "c"
-    while(chooseExit == "c" ):
+    while(chooseExit == "c"):
         os.system("clear")
         print("\n\tBelow are the available images\n")
         os.system("docker ps -a")
-        deleteDockerImageChoice = input(" ENTER 1 to delete a single images \n ENTER 2 to delete all running images \n ENTER 3 to delete all the docker images\n ")
+        deleteDockerImageChoice = input(
+            " ENTER 1 to delete a single images \n ENTER 2 to delete all running images \n ENTER 3 to delete all the docker images\n ")
 
-        if(deleteDockerImageChoice =="3"):
+        if(deleteDockerImageChoice == "3"):
             deleteAllFlag = 1
             print("\n\tDeleting all docker Image\n")
             deleteAllDockerImage()
-            
+
         elif(deleteDockerImageChoice == "1"):
             print("\n\tDeleting single IMAGE\n")
             deleteDockerImageChoiceFunction()
-            
+
         elif(deleteDockerImageChoice == "2"):
             deleteAllRunningDockerImage()
-            
+
         else:
             print("\n\t\t WRONG OPTION\n")
         chooseExit = input("\n\tENTER c to continue\n\tENTER e to exit\n")
         if(chooseExit == "e"):
             return
-# 💥 here I'm having trouble exiting the code if wrong ImageName is given          
-    
+# 💥 here I'm having trouble exiting the code if wrong ImageName is given
+
     print("\n\tYou have entered wrong option ")
     choice = input("Enter C to continue and E to exit\t------------>\t")
     if(choice != "c" and deleteAllFlag == 1):
@@ -233,41 +237,45 @@ def deleteDockerImage():
     else:
         configDockerImage(dockerStart)
 # ------------------>>>>>>deleteAllDockerImage()<<<_------------------
+
+
 def deleteAllDockerImage():
     print("\n\tDeleting all docker Images")
-    os.system("docker rm `docker ps -a -q`")       
-    
-    #-------------------------->>>>>>>>>>>>>>    deleteDockerImage()   <<<<<<--------------------
+    os.system("docker rm `docker ps -a -q`")
 
-    
+    # -------------------------->>>>>>>>>>>>>>    deleteDockerImage()   <<<<<<--------------------
+
+
 def deleteDockerImageChoiceFunction():
     imageName = input("\n\tEnter the name of the image you want to remove")
     print("\n\t\t\t REMOVING {}\n".format(imageName))
     os.system("docker rm {}".format(imageName))
-    
-#-------------------------->>>>>>>>>>>>>>    deleteAllDockerImage()   <<<<<<--------------------
+
+# -------------------------->>>>>>>>>>>>>>    deleteAllDockerImage()   <<<<<<--------------------
+
+
 def deleteAllDockerImage():
     print("\n\tDeleting all docker Images")
-    os.system("docker rm `docker ps -a -q`")            
+    os.system("docker rm `docker ps -a -q`")
 
-#-------------------------->>>>>>>>>>>>>>    deleteAllRunningDockerImage()   <<<<<<--------------------
+# -------------------------->>>>>>>>>>>>>>    deleteAllRunningDockerImage()   <<<<<<--------------------
+
 
 def deleteAllRunningDockerImage():
     print("\n\tDeleting all running images")
     os.system("docker rm `docker ps -q`")
 
 
-
-#-------------------------->>>>>>>>>>>>>>    docker()   <<<<<<--------------------
+# -------------------------->>>>>>>>>>>>>>    docker()   <<<<<<--------------------
 
 def docker():
     os.system("clear")
     print("\n\t Welcome to dockers below are the list of options to use docker \t!!")
-    
+
     service()
     Rechoice()
- 
+
 # ---------------->>>>>>>>>>>>>>>>>>   Here only by calling docker function the whole program will run
 
 
-#docker()  #<<<<<<<----------- call docker() to start the program
+# docker()  #<<<<<<<----------- call docker() to start the program
